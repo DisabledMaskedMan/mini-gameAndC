7dc9afd8e6004af37f474eceff848d1c01cb888d
ZY ZHANG <121227562+DisabledMaskedMan@users.noreply.github.com>
2023-07-24 20:17:28 +0800

    Delete 光敏传感器控制蜂鸣器 directory

--- 光敏传感器控制蜂鸣器/Hardware/Light_Sensor.h
+++
@@ -1,7 +0,0 @@
-#ifndef __Light_Sensor_H_
-#define __Light_Sensor_H_
-
-void GM_Init(void);
-uint8_t GM_Num(void);
-
-#endif
--- 光敏传感器控制蜂鸣器/Hardware/buzzer.h
+++
@@ -1,8 +0,0 @@
-#ifndef __buzzer_H_
-#define __buzzer_H_
-
-void Buzzer_Init(void);
-void Buzzer_ON(void);
-void Buzzer_Off(void);
-
-#endif
